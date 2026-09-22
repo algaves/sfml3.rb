18aad10e597191e9e90282e5e3abbc86dc6324f7
sealtielfreak <sealtielfreak@yandex.com>
2026-09-21 19:17:13 -0600

    test(rubyesque): cover Style/State, the Class[...] constructors and window forms
    Exercise the flag namespaces, VideoMode/Vector/Color/Rect/Time/View and
    shape bracket constructors, and assert the window loop/block methods land
    on the right owner without instantiating a window.

--- test/rubyesque_test.rb
+++ test/rubyesque_test.rb
@@ -137,4 +137,85 @@ class RubyesqueTest < Minitest::Test
 
     assert_equal event.key[:code], event.code
   end
+
+  def test_style_and_state_are_integer_flag_namespaces
+    assert_equal [0, 1, 2, 4, 7],
+                 [Style::NONE, Style::TITLEBAR, Style::RESIZE, Style::CLOSE, Style::DEFAULT]
+    assert_equal Style::TITLEBAR | Style::RESIZE | Style::CLOSE, Style::DEFAULT
+
+    assert_equal 0, State::WINDOWED
+    assert_equal 1, State::FULLSCREEN
+  end
+
+  def test_video_mode_bracket_constructor
+    mode = VideoMode[640, 480, 32]
+
+    assert_kind_of VideoMode, mode
+    assert_equal [640, 480], [mode.width, mode.height]
+    assert_equal 32, mode.bits
+    assert_equal 32, VideoMode[800, 600].bits
+  end
+
+  def test_vector_bracket_constructors
+    assert_equal [1.0, 2.0], Vector2[1, 2].to_a
+    assert_equal [3.0, 4.0], Vector2[[3, 4]].to_a
+    assert_equal [1.0, 2.0, 3.0], Vector3[1, 2, 3].to_a
+    assert_equal [4.0, 5.0, 6.0], Vector3[[4, 5, 6]].to_a
+  end
+
+  def test_color_rect_and_time_bracket_constructors
+    assert_equal [1, 2, 3, 255], Color[1, 2, 3].to_a
+    assert_equal Color.from_integer(0x11223344), Color[0x11223344]
+    assert_equal [0.0, 0.0, 10.0, 20.0], Rect[0, 0, 10, 20].to_a
+    assert_equal [1.0, 2.0, 0.0, 0.0], Rect[1, 2].to_a
+
+    assert_in_epsilon 0.5, SFML::Time[0.5].as_seconds, 1e-9
+    assert_in_epsilon 2.0, SFML::Time[2].as_seconds, 1e-9
+  end
+
+  def test_view_bracket_constructor
+    assert_vec_in_epsilon [10, 20], View[Rect[0, 0, 10, 20]].size.to_a
+    assert_vec_in_epsilon [5, 10], View[[0, 0, 10, 20]].center.to_a
+
+    assert_vec_in_epsilon [5, 6], View[[5, 6], [10, 20]].center.to_a
+    assert_vec_in_epsilon [10, 20], View[[5, 6], [10, 20]].size.to_a
+
+    assert_raises(ArgumentError) { View[1, 2, 3] }
+    assert_raises(ArgumentError) { View[[1, 2]] }
+  end
+
+  def test_shape_bracket_constructors
+    circle = CircleShape[30, [100, 100]]
+
+    assert_in_epsilon 30, circle.radius, 1e-5
+    assert_vec_in_epsilon [100, 100], circle.position.to_a
+    assert_vec_in_epsilon [0, 0], CircleShape[5].position.to_a
+
+    rectangle = RectangleShape[10, 20, 100, 40]
+    assert_vec_in_epsilon [10, 20], rectangle.position.to_a
+    assert_vec_in_epsilon [100, 40], rectangle.size.to_a
+
+    polygon = ConvexShape[[0, 0], [100, 0], [50, 80]]
+    assert_equal 3, polygon.point_count
+    assert_vec_in_epsilon [100, 0], polygon.point(1).to_a
+  end
+
+  def test_resource_bracket_constructors_are_declared
+    assert_respond_to Sprite, :[]
+    assert_respond_to Texture, :[]
+    assert_respond_to Image, :[]
+    assert_respond_to RenderTexture, :[]
+    assert_respond_to Text, :[]
+
+    assert_vec_in_epsilon [4, 4], Image[[4, 4]].size.to_a
+  end
+
+  def test_window_block_forms_are_declared
+    assert_equal WindowBase, WindowBase.instance_method(:open!).owner
+    assert_equal Window, Window.instance_method(:poll_event!).owner
+    assert_equal Window, Window.instance_method(:wait_event!).owner
+
+    assert_includes WindowBase.instance_method(:poll_event!).parameters, %i[opt event]
+    assert_includes WindowBase.instance_method(:wait_event!).parameters, %i[opt event]
+  end
 end
